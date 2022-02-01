1962084d2a37b1165cc2e974476ae47daae506fe
Bongani <bearnomangola7@gmail.com>
2022-02-01 15:18:22 +0200

    edits

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -48,18 +48,17 @@
         </ul>
 
         <hr>
-        <ul class="social-media-list">
-          <li>
-            <a href="https://github.com/Teddy1824" target="_blank" class="contact-icon">
-            <i class="fab fa-github" aria-hidden="true"></i></a>
-          </li>
-          <li><a href="https://codepen.io/teddy1824" target="_blank" class="contact-icon">
-            <i class="fab fa-codepen" aria-hidden="true"></i></a>
-          </li>
-          <li><a href="https://www.linkedin.com/in/bongani-nomangola-78b92a223/" target="_blank" class="contact-icon">
-            <i class="fab fa-linkedin-in" aria-hidden="true"></i></a>
-          </li>
-          </ul>
+        <div class="social-icons">
+  <a class="social-icon social-icon--codepen" href="https://codepen.io/teddy1824" target="_blank">
+  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--akar-icons" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" data-icon="akar-icons:codepen-fill"><g fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M11.372.19c.38-.253.875-.253 1.256 0L23.492 7.4c.317.21.508.565.508.946v7.308c0 .38-.19.736-.508.947l-10.864 7.21c-.38.252-.875.252-1.256 0L.508 16.6A1.136 1.136 0 0 1 0 15.654V8.346c0-.38.19-.736.508-.947L11.372.19zm-9.1 10.273v3.058l2.288-1.54l-2.288-1.518zm4.337 2.878L3.18 15.648l7.684 5.1v-4.583L6.61 13.341zm6.527 2.824v4.582l7.684-5.1l-3.43-2.306l-4.254 2.824zm6.303-4.183l2.29 1.54v-3.06l-2.29 1.52zm1.371-3.636l-3.41 2.263l-4.264-2.868V3.253l7.674 5.093zm-9.946-5.093V7.74l-4.263 2.868L3.19 8.346l7.674-5.093zM12 9.715l-3.35 2.254L12 14.192l3.35-2.223L12 9.715z" fill="currentColor"></path></g></svg>
+  </a>
+  <a class="social-icon social-icon--github" href="https://github.com/Teddy1824" target="_blank">
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--line-md" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" data-icon="line-md:github"><g fill="none"><path d="M12 4c1.668 0 2.612.4 3 .5c.525-.425 1.938-1.5 3.5-1.5c.344 1 .286 2.22 0 3c.75 1 1 2 1 3.5c0 2.188-.483 3.582-1.5 4.5c-1.017.918-2.111 1.375-3.5 1.5c.65.538.5 1.874.5 2.5v3" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="il-md-length-40 il-md-duration-3 il-md-delay-0"></path><path d="M12 4c-1.668 0-2.612.4-3 .5C8.475 4.075 7.062 3 5.5 3c-.344 1-.286 2.22 0 3c-.75 1-1 2-1 3.5c0 2.188.483 3.582 1.5 4.5c1.017.918 2.111 1.375 3.5 1.5c-.65.538-.5 1.874-.5 2.5v3" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="il-md-length-40 il-md-duration-3 il-md-delay-0"></path><path d="M9 19c-1.406 0-2.844-.563-3.688-1.188C4.47 17.188 4.22 16.157 3 15.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" class="il-md-length-15 il-md-duration-2 il-md-delay-3"></path></g></svg>
+  </a>
+  <a class="social-icon social-icon--linkedin" href="https://www.linkedin.com/in/bongani-nomangola-78b92a223/" target="_blank">
+  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--line-md" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" data-icon="line-md:linkedin"><g fill="none"><path d="M4 10v10" stroke="currentColor" stroke-width="4" stroke-linecap="round" class="il-md-length-15 il-md-duration-2 il-md-delay-3"></path><circle cx="4" cy="4" r="2" fill="currentColor" class="il-md-fill il-md-delay-0"></circle><path d="M10 20V10" stroke="currentColor" stroke-width="4" stroke-linecap="round" class="il-md-length-15 il-md-duration-2 il-md-delay-5"></path><path d="M10 15a5 5 0 0 1 10 0v5" stroke="currentColor" stroke-width="4" stroke-linecap="round" class="il-md-length-25 il-md-duration-2 il-md-delay-7"></path></g></svg>
+  </a>
+</div>
          <hr>
 
         <div class="copyright">&copy; ALL OF THE RIGHTS RESERVED</div>
@@ -195,9 +194,53 @@ textarea {
   background-color: lightgray;
 }
 
+.social-icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    width: 60px;
+    height: 60px;
+    margin: 0 0.5rem;
+    margin-left: 7px;
+    border-radius: 50%;
+    cursor: pointer;
+    font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+    font-size: 2.5rem;
+    text-decoration: none;
+    transition: all 0.15s ease;
+}
 
+.social-icon--codepen {
+    background: grey;
+    color: #fff;
+}
+
+.social-icon--github {
+    background: grey;
+    color: #fff;
+}
+
+.social-icon--linkedin {
+    background: grey;
+    color: #fff;
+}
+
+.social-icons {
+    flex-wrap: wrap;
+    display: flex;
+    justify-content: center;
+}
+
+.social-icon :hover {
+  color: white;
+}
+
+.social-icon a :hover {
+  color: white;
+}
 /* Social Media Icons */
-.social-media-list {
+/* .social-media-list {
   position: relative;
   font-size: 22px;
   text-align: center;
@@ -250,7 +293,7 @@ textarea {
 
 .social-media-list li:hover a {
   color: #000;
-}
+} */
 
 .copyright {
   font: 200 14px 'Oswald', sans-serif;
