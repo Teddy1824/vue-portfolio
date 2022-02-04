f1ec6abfea82e7a79fff6a1fb38bac6394d6869f
Bongani <bearnomangola7@gmail.com>
2022-02-04 14:16:11 +0200

    changes

--- src/views/ProjectD.vue
+++ src/views/ProjectD.vue
@@ -17,16 +17,14 @@ export default {
     {"title":"Calculator", "id":"5", "img":"https://i.postimg.cc/4y8Hg2Pq/Screenshot-from-2022-01-26-11-37-27.png", "desc":"", "lang":"HTML, CSS, BOOTSTRAP, JavaScript", "live":"https://bncalculator.netlify.app", "github":"https://github.com/Teddy1824/Vue.calculator"},
     {"title":"Mercedes Benz Intro page", "id":"6", "img":"https://i.postimg.cc/3NVmbx0t/Screenshot-from-2022-01-31-11-45-40.png", "desc":"", "lang":"HTML, CSS, BOOTSTRAP", "live":"https://objective-pasteur-77172a.netlify.app/", "github":"https://github.com/Teddy1824/Media-Query"},
     {"title":"Reaction-Time Game", "id":"7", "img":"https://i.postimg.cc/SR00wfy0/Screenshot-from-2022-02-01-09-22-05.png", "desc":"", "lang":"HTML, CSS, Vue.js", "live":"https://naughty-bell-8b8d72.netlify.app/", "github":"https://github.com/Teddy1824/Vue.reaction-time"}],
-
-          project: null
       }
   },
-    mounted() {
-      fetch('http://localhost:3000/projects/' + this.id)
-        .then(res => res.json())
-        .then(data => this.job = data)
-        .catch(err => console.log(err.message))
-  },
+  //   mounted() {
+  //     fetch('http://localhost:3000/projects/' + this.id)
+  //       .then(res => res.json())
+  //       .then(data => this.job = data)
+  //       .catch(err => console.log(err.message))
+  // },
 }
 </script>
 
